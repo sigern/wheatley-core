0f99e44f1f443a43801ab9e133de3229d7170bbf
Kacper Landa <sigern@gmail.com>
2023-03-23 23:59:07 +0100

    Frame parser thread

--- include/defines.h
+++ include/defines.h
@@ -3,12 +3,49 @@
 #ifndef __DEFINES_H
 #define __DEFINES_H
 
+#include "stm32f4xx_hal.h"
+
+#define JOYSTICK_MIN  0
+#define JOYSTICK_ZERO 120
+#define JOYSTICK_MAX  240
+
 enum EFrame {
     FRAME_START = 0xF0,
     FRAME_END   = 0xF1,
     FRAME_TYPE_JOYSTICK = 0xF2,
-    FRAME_TYPE_SERVO_VALUE = 0xF3
+    FRAME_TYPE_SERVO = 0xF3,
+    FRAME_TYPE_LIPOL = 0xF4,
+    FRAME_TYPE_VELOCITY = 0xF5
 };
+
+enum EReceiverState {
+    NONE,
+    CHECK_TYPE,
+	  CHECK_END,
+    JOYSTICK_TILT,
+	  JOYSTICK_ROLL
+};
+
+typedef struct joystickState
+{
+    uint8_t tilt;
+    uint8_t roll;
+} JoystickState_t;
+
+typedef struct robotState
+{
+    uint16_t tilt_servo;
+    uint16_t roll_servo;
+    float velocity;
+    float lipol_vol;
+    float gain_p;
+    float gain_i;
+    float gain_d;
+} RobotState_t;
+
+
+volatile RobotState_t g_wheatley = {0u, 0u, 0.f, 0.f, 0.f, 0.f, 0.f};
+volatile JoystickState_t g_joystick = {JOYSTICK_ZERO, JOYSTICK_ZERO};
 
 #endif /* __DEFINES_H */
 
--- src/Threads.c
+++ src/Threads.c
@@ -13,6 +13,7 @@ static osThreadId_t tid_thrLEDBlinker;           // Thread id of thread: LEDBlin
 static osThreadId_t tid_thrServoModulator;       // Thread id of thread: ServoModulator
 static osThreadId_t tid_thrFrameParser;          // Thread id of thread: FrameParser
 static osThreadId_t tid_thrSender;               // Thread id of thread: Sender
+static osThreadId_t tid_thrController;           // Thread id of thread: Controller
 
 extern UART_HandleTypeDef huart6;
 extern uint8_t UART6_rxBuffer;
@@ -46,23 +47,71 @@ __NO_RETURN void thrServoModulator (void *argument) {
   thrFrameParser: Parse frame received via UART
  *----------------------------------------------------------------------------*/
 __NO_RETURN void thrFrameParser (void *argument) {
-	uint8_t msg;
+	uint8_t receiverState = NONE;
+	uint8_t msg = 0u;
+	uint8_t tilt_cache = JOYSTICK_ZERO;
+	uint8_t roll_cache = JOYSTICK_ZERO;
 	osStatus_t status;
+	
   for (;;) {
 		status = osMessageQueueGet(UartRxMsgQueueId, &msg, NULL, NULL);  // wait for message
-		uint32_t input[] = {0x01020304, 0x05060708};
-		uint32_t crc = 0;
-		uint8_t textCrc[4] = {0};
 		if (status == osOK) {
-			crc = CRC_Calculate(&input, 2);
-		   uint8_t *ptr = (uint8_t*)&crc;
-			for(int i=0; i<4; ++i, ++ptr)
-			{
-				textCrc[i] = *ptr;
-	  	}
-			//Bluetooth_Send(textCrc, 4);
+      switch(receiverState) {
+        case NONE:
+          if (msg == FRAME_START) {
+            receiverState = CHECK_TYPE;
+          }
+          break;
+        case CHECK_TYPE:
+          if (msg == FRAME_TYPE_JOYSTICK) {
+						receiverState = JOYSTICK_TILT;
+          } else {
+            receiverState = NONE;
+          }
+          break;
+        case JOYSTICK_TILT:
+					tilt_cache = (uint8_t)msg;
+          receiverState = JOYSTICK_ROLL;
+          break;
+				case JOYSTICK_ROLL:
+          roll_cache = (uint8_t)msg;
+          receiverState = CHECK_END;
+          break;
+				case CHECK_END:
+					if (msg == FRAME_END) {
+						g_joystick.tilt = tilt_cache;
+						g_joystick.roll = roll_cache;
+          }
+          receiverState = NONE;
+          break;
+        default:
+          receiverState = NONE;
+          break;
+      }
 		}
-  }
+		
+		if(g_joystick.tilt < 60) {
+			LED_Off(RED);
+			LED_On(GREEN);
+    } else if (g_joystick.tilt > 180) {
+			LED_Off(GREEN);
+			LED_On(RED);
+		} else {
+			LED_Off(RED);
+			LED_Off(GREEN);
+		}
+		
+		if(g_joystick.roll < 60) {
+			LED_Off(ORANGE);
+			LED_On(BLUE);
+    } else if (g_joystick.roll > 180) {
+			LED_Off(BLUE);
+			LED_On(ORANGE);
+		} else {
+			LED_Off(BLUE);
+			LED_Off(ORANGE);
+		}
+	}
 }
 
 /*------------------------------------------------------------------------------
@@ -72,8 +121,8 @@ __NO_RETURN void thrSender (void *argument) {
 	uint16_t testTilt = 0;
 	uint16_t testRoll = 0;
 	uint8_t testFrame[] = {
-		FRAME_START, 
-	  FRAME_TYPE_SERVO_VALUE, 
+    FRAME_START, 
+	  FRAME_TYPE_SERVO, 
 	  (uint8_t)(testTilt & 0xFF),
 	  (uint8_t)(testTilt >> 8 & 0xFF), 
 	  (uint8_t)(testRoll & 0xFF), 
@@ -81,14 +130,15 @@ __NO_RETURN void thrSender (void *argument) {
     FRAME_END
 	};
   for (;;) {
-		osDelay (200U);  // Delay 2000 ms
+		uint32_t queueSize = osMessageQueueGetCount(UartRxMsgQueueId);
+		osDelay (100);  // Delay 2000 ms
 		Bluetooth_Send(testFrame, sizeof(testFrame));
 		testTilt++;
 		testRoll++;
-		testFrame[2] = (uint8_t)(testTilt >> 8 & 0xFF);
-		testFrame[3] = (uint8_t)(testTilt & 0xFF);
-		testFrame[4] = (uint8_t)(testRoll >> 8 & 0xFF);
-		testFrame[5] = (uint8_t)(testRoll & 0xFF);
+		testFrame[2] = (uint8_t)(queueSize >> 8 & 0xFF);
+		testFrame[3] = (uint8_t)(queueSize & 0xFF);
+		testFrame[4] = (uint8_t)(0 >> 8 & 0xFF);
+		testFrame[5] = (uint8_t)(g_joystick.roll & 0xFF);
   }
 }
 
